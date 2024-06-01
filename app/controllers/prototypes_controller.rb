ca8db48dabd657c2ad54ae5a0853bfd8a6d97673
azu-M888 <machine27ft@gmail.com>
2024-06-01 22:18:12 +0900

    prototype関連の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -19,9 +19,23 @@ class PrototypesController < ApplicationController
   end
 
   def edit
+    @prototype = Prototype.find(params[:id])
   end
 
   def update
+    @prototype = Prototype.find(params[:id])
+  
+    if @prototype.update(prototype_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
  root to: 'prototypes#index'
  resources :users, only: [:edit, :update]
- resources :prototypes, only: [:index,:new,:create,:show,:edit,:update]
-
+ resources :prototypes
 end
