0a0845336c54f68d2232be534e3750429acdfc8e
azu-M888 <machine27ft@gmail.com>
2024-06-06 20:49:15 +0900

    デプロイ_issu修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -27,6 +27,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
@@ -54,7 +57,7 @@ class PrototypesController < ApplicationController
   
   def move_to_index
     unless user_signed_in?
-      redirect_to action: :index
+      redirect_to user_session_path
     end
   end
 end
